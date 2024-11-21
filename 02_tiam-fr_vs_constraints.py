eb4e1c7915de6e6bc8b8427691f5e8aab58a972b
corralien <damien.corral@gmail.com>
2024-11-21 15:29:24 +0100

    Initial version

---
+++ 02_tiam-fr_vs_constraints.py
@@ -0,0 +1,270 @@
+# %%
+import pandas as pd
+import pathlib
+import sqlite3
+
+DATADIR = pathlib.Path('data')
+conn = sqlite3.connect(DATADIR / 'c1.db')
+
+# %% [markdown]
+# ## GHG
+
+# %%
+qs = """SELECT
+   Scenario,
+   Period as Year,
+   SUM(
+   CASE
+      WHEN
+         commodity = 'GHG'
+      THEN
+         PV
+   END)/ 1000000 AS Value
+FROM
+   Var_Comnet
+GROUP BY
+   Scenario, Period;"""
+
+df_ghg = pd.read_sql_query(qs, conn)
+
+# %%
+df_ghg
+
+# %% [markdown]
+# ## CCSFOS
+
+# %%
+qs = """SELECT
+   Scenario,
+   Period AS 'Year',
+   SUM(PV)/1000000 AS 'Value'
+FROM
+   VAR_FOut
+WHERE
+   Process IN ('CCSDUMELCN', 'CCSDUMINDN', 'CCSDUMINDP', 'CCSDUMSUPN')
+GROUP BY Scenario, Period
+
+UNION
+
+SELECT
+   Scenario,
+   '2020' AS 'Year',
+   '0' AS Value
+FROM
+   VAR_FOut
+WHERE
+   Process IN ('CCSDUMELCN', 'CCSDUMINDN', 'CCSDUMINDP', 'CCSDUMSUPN')
+GROUP BY
+   Scenario, Year
+ORDER BY
+   Scenario, Year;"""
+
+df_ccsfos = pd.read_sql_query(qs, conn)
+
+# %%
+df_ccsfos
+
+# %% [markdown]
+# ## LCSPE
+
+# %%
+qs = """SELECT
+   Scenario,
+   Period AS Year,
+   'Low-carbon' AS 'Label',
+   SUM(PV) AS Value
+FROM
+   VAR_FIn
+WHERE 
+   Process NOT LIKE ('TU_BIO%') AND 
+   Commodity IN ('AGRSOL','COMSOL','RESSOL','ELCSOL',
+                 'ELCNUC','ELCTDL','ELCWAV','ELCWIN',
+                 'GEO','ELCHYD','INDHYD','INDREN','INDWIN',
+                 'BIOARSH','BIOARSP','BIOBIN','BIOBMU',
+                 'BIOCRP','BIOLOG','BIOOIL','BIOPRC',
+                 'BIOWOOD')
+GROUP BY
+   Scenario,Label,Year
+
+UNION ALL
+
+SELECT
+   Scenario,
+   Period AS Year,
+   'Fossil' AS 'Label',
+   SUM(PV) AS Value 
+FROM
+   VAR_FIn 
+WHERE 
+   Process NOT LIKE ('TU_%') AND
+   Process NOT LIKE ('UPR%') AND
+   Commodity IN ('GASNGA','OILCRD','OILNGL',
+                 'COABCO','COAHCO','MWSTNR')
+GROUP BY
+   Scenario,Label,Year
+
+UNION ALL
+
+SELECT
+   Scenario,
+   Period AS Year,
+   'Low-carbon' AS 'Label',
+   SUM(PV) AS Value 
+FROM
+   Var_FOut
+WHERE
+   Process IN ('UBIOSRCSLD100','UBIOSRCLIG100','UBIOCAGLIG100','UBIOMISLIG100','UBIOSWTLIG100')
+GROUP BY
+   Scenario,Label,Year
+
+UNION ALL
+
+SELECT
+   Scenario,
+   Period AS Year,
+   'Low-carbon' AS 'Label',
+   SUM(PV) AS Value 
+FROM
+   VAR_FIn
+WHERE
+   Commodity IN ('ELCCOA','ELCNGA','INDBFG','INDCOA','INDCOG','INDCOK','INDHFO','INDLPG',
+                 'INDMWST','INDNGA','INDOIL','INDOVC','INDPTC','SUPCOA','SUPNGA',
+                 'IISCOK','IISNGAS','IISCOA')
+   AND (Process IN ('IISBCHCKBFCS01','IISCHRCAUD01',
+                   'IISCHRCOAMXCS01','IISCOABFCS01',
+                   'IISCRXCS01','IISCRXCS02',
+                   'IISNGASBFCS01','IISNGBGUD01',
+                   'IISNGBSNGMXCS01')
+   OR Process LIKE 'HZ%'
+   OR Process LIKE 'E%CC'
+   OR Process LIKE 'EZ%'
+   OR Process LIKE 'INM%MIX%CC')
+GROUP BY
+   Scenario,Label,Year;"""
+
+df_lcspe = pd.read_sql_query(qs, conn)
+
+# %%
+df_lcspe = (df_lcspe.pivot_table(index=['Scenario', 'Year'], columns='Label', values='Value', aggfunc='sum')
+                    .assign(Value=lambda x: x['Low-carbon'] / (x.pop('Low-carbon') + x.pop('Fossil')))
+                    .reset_index().rename_axis(columns=None))
+df_lcspe
+
+# %% [markdown]
+# ## FED
+
+# %%
+qs = """SELECT 
+   Scenario, 
+   Period as 'Year',
+   Process,
+   SUM(Pv)/1000 as 'Value' 
+FROM 
+   Var_Fin 
+GROUP BY
+   Scenario,
+   Process,
+   Period;"""
+
+df_fed = pd.read_sql_query(qs, conn)
+
+# %%
+dmap = pd.read_csv(DATADIR / 'mapping.csv', sep=';').set_index('Commodity')['Label']
+label = df_fed.pop('Process').map(dmap)
+df_fed = df_fed.loc[label == 'Final energy process']
+df_fed = df_fed.groupby(['Scenario', 'Year'], as_index=False).sum()
+df_fed
+
+# %% [markdown]
+# ## CO2ELC
+
+# %%
+qs = """SELECT
+   Scenario,
+   Period AS Year,
+   Commodity,
+   SUM(PV) AS Value
+FROM
+   VAR_FOut
+WHERE
+   Commodity IN ('ELC')
+GROUP BY
+   Scenario, Period, Commodity
+
+UNION
+
+SELECT
+   Scenario,
+   Period AS Year,
+   Commodity,
+   SUM(PV) AS Value
+FROM
+   VAR_Comnet
+WHERE
+   Commodity IN ('ELCCO2N')
+GROUP BY
+   Scenario, Period, Commodity;"""
+
+df_co2elc = pd.read_sql_query(qs, conn)
+
+# %%
+df_co2elc = (df_co2elc.pivot_table(index=['Scenario', 'Year'], columns='Commodity', values='Value')
+                      .assign(Value=lambda x: x.pop('ELCCO2N') / x.pop('ELC') * 3.6)  # x3.6 to transform MWh to PJ
+                      .reset_index().rename_axis(columns=None))
+df_co2elc
+
+# %% [markdown]
+# ## ESFE
+
+# %%
+qs = """SELECT 
+   Scenario,
+   Period as 'Year',
+   SUM(PV)/1000 as 'ELCFIN'
+FROM VAR_FIn
+WHERE Process IN ('FT_INDELC', 'FT_AGRELC', 'FT_COMELC', 'FT_RESELC', 'FT_TRAELC')
+GROUP BY Scenario, Period;"""
+
+df_esfe = pd.read_sql_query(qs, conn)
+
+# %%
+df_esfe = (df_esfe.merge(df_fed, on=['Scenario', 'Year'])
+                  .assign(Value=lambda x: x.pop('ELCFIN') / x['Value']))
+df_esfe
+
+# %% [markdown]
+# ## NONNRG
+
+# %%
+qs = """SELECT
+   Scenario,
+   Period as Year,
+   SUM(
+   CASE
+      WHEN
+         Commodity IN ('NONNRG') 
+      THEN
+         PV 
+   END)/ 1000000 AS Value
+FROM
+   Var_Comnet 
+GROUP BY
+   Scenario, Period;"""
+
+df_nonnrg = pd.read_sql_query(qs, conn)
+
+# %%
+df_nonnrg
+
+# %% [markdown]
+# ## Export
+
+# %%
+constraints = pd.read_csv(DATADIR / 'constraints.csv', index_col=['Scenario', 'Year'])
+
+dfs = {'ghg': df_ghg, 'lcspe': df_lcspe, 'fed': df_fed, 'esfe': df_esfe,
+       'co2elc': df_co2elc, 'ccsfos': df_ccsfos, 'nonnrg': df_nonnrg}
+
+df = pd.concat({var: df.loc[df['Year'] != '2018'].set_index(['Scenario', 'Year'])['Value'] for var, df in dfs.items()}, axis=1)
+df = pd.concat([constraints, df], axis=0, sort=False)[dfs.keys()]
+df.to_csv(DATADIR / 'tiam-fr_vs_constraints.csv')
---
+++ 04_paper_fig2-3.py
@@ -0,0 +1,188 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent,ipynb
+#     notebook_metadata_filter: language_info
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.10.13
+# ---
+
+# %% [markdown]
+# # Figures
+
+# %% editable=true slideshow={"slide_type": ""}
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as mpe
+import scienceplots
+import pathlib
+
+DATADIR = pathlib.Path('data')
+OUTPUT = pathlib.Path('figures')
+
+mpl.style.use(["science", "nature"])
+
+# %%
+df = pd.read_csv(DATADIR / 'tiam-fr_vs_constraints.csv', parse_dates=['Year'])
+
+filtered = ['base']
+df = df[~df['Scenario'].isin(filtered)]
+
+gmap = {
+    r'^IMP-.*': 'imp',
+    r'Median|\d+th|base': 'stat',
+    r'^[bgc].*|tab2': 'emissions',
+    r'^[nf].*|tab4': 'energy',
+}
+df['Group'] = df['Scenario'].replace(gmap, regex=True)
+
+# %% [markdown]
+# ### Règles d'affichage
+#
+# La médiane et les IMP-* sont toujours au dessus de toutes les autres courbes. Les faisceaux sont toujours en dessous : 5-95th < budget < energy < trend
+
+# %% editable=true slideshow={"slide_type": ""}
+chart_elements = {
+    'ghg': {'title': 'GHG emissions', 'ylabel': 'GtCO$_{2,eq}$', 'fig_id': None},
+    'lcspe': {'title': 'Low-carbon share of primary energy', 'ylabel': 'Ratio', 'fig_id': 'a'},
+    'co2elc': {'title': 'CO$_2$ intensity of electricity', 'ylabel': 'MtCO$_2$.TWh$^{-1}$', 'fig_id': 'b'},
+    'esfe': {'title': 'Electricity share of final energy', 'ylabel': 'Ratio', 'fig_id': 'c'},
+    'fed': {'title': 'Final energy demand', 'ylabel': 'EJ', 'fig_id': 'd'},
+    'ccsfos': {'title': 'Fossil CCS', 'ylabel': 'GtCO$_2$', 'fig_id': 'e'},
+    'nonnrg': {'title': 'Non-energy GHG emissions', 'ylabel': 'GtCO$_{2,eq}$', 'fig_id': 'f'}
+}
+
+kwds = {
+    # Default parameters for each group
+    "stat": {"color": "lightgray", "zorder": 100},
+    "imp": {"path_effects": [mpe.Stroke(linewidth=2, foreground='black'), mpe.Normal()]},
+    "emissions": {"color": "lightblue", "linewidth": 0.8, "linestyle": "dotted"},
+    "energy": {"color": "lightcoral", "linewidth": 0.8, "linestyle": "dotted"},
+
+    # Groupe STAT
+    'Median': {"color": "black", "linewidth": 1.5, "linestyle": "solid"},
+    '5th': {"color": "gray", "linewidth": 0.5, "linestyle": "dotted"},
+    '95th':  {"color": "gray", "linewidth": 0.5, "linestyle": "dotted"},
+
+    # Groupe IMP (Illustrative mitigation pathways)
+    'IMP-LD': {"color": "#daa258", "linewidth": 1.2},  # C1
+    'IMP-Ren': {"color": "#b4b8d9", "linewidth": 1.2},  # C1
+    'IMP-SP': {"color": "#e5cf00", "linewidth": 1.2},  # C1
+    'IMP-Neg': {"color": "#a7b18f", "linewidth": 1.2},  # C2
+    'IMP-GS': {"color": "#629ebf", "linewidth": 1.2},  # C3
+    'IMP-Neg-2.0': {"color": "#e5cf00", "linewidth": 1.2},  # C3
+    'IMP-Ren-2.0': {"color": "#b4b8d9", "linewidth": 1.2},  # C3
+
+    # Groupe EMISSIONS (bleu)
+    "tab2": {"color": "darkblue", "linestyle": "solid", "linewidth": 1},
+   
+    # Groupe ENERGY (rouge)
+    "tab4": {"color": "darkred", "linestyle": "solid", "linewidth": 1},
+}
+
+
+# %%
+def make_plot(var, df, fig_id):
+    width, height = mpl.rcParams["figure.figsize"]
+    fig, ax = plt.subplots(figsize=(width, height), dpi=300, constrained_layout=True)
+    
+    ax.set_title(chart_elements[var]['title'])
+    if fig_id:
+        ax.set_title(f'({fig_id})', loc='left', fontsize=10)
+
+    piv = df.pivot_table(index=['Group', 'Scenario'], columns='Year', values=var)
+    grp = piv.groupby('Group')
+
+    # Legend
+    lines = {}
+    area = {}
+    
+    # Groupe STAT
+    df1 = grp.get_group('stat').droplevel('Group')
+    lines['Median'] = ax.plot(df1.loc['Median'], **(kwds['stat'] | kwds['Median']))
+    lines['5th'] = ax.plot(df1.loc['5th'], **(kwds['stat'] | kwds['5th']))
+    lines['95th'] = ax.plot(df1.loc['95th'], **(kwds['stat'] | kwds['95th']))
+    area['stat'] = ax.fill_between(df1.columns, df1.loc['5th'], df1.loc['95th'], color=kwds["stat"]["color"], alpha=0.5)
+    
+    # Groupe IMP
+    df1 = grp.get_group('imp').droplevel('Group')
+    for scen, data in df1.iterrows():
+        lines[scen] = ax.plot(data, **(kwds['imp'] | kwds[scen]))
+    
+    # Groupe ENERGY
+    df1 = grp.get_group('energy').droplevel('Group')
+    area['energy'] = ax.fill_between(df1.columns, df1.min(axis=0), df1.max(axis=0), color=kwds["energy"]["color"], alpha=0.5)
+    lines['tab4'] = ax.plot(df1.loc['tab4'], **(kwds['energy'] | kwds['tab4']))
+    
+    # Groupe EMISSIONS
+    df1 = grp.get_group('emissions').droplevel('Group')
+    area['emissions'] = ax.fill_between(df1.columns, df1.min(axis=0), df1.max(axis=0), color=kwds["emissions"]["color"], alpha=0.5)
+    lines['tab2'] = ax.plot(df1.loc['tab2'], **(kwds['emissions'] | kwds['tab2']))
+    
+    ax.set_ylabel(chart_elements[var]['ylabel'])
+    ax.tick_params(axis='x', labelrotation=45)
+    ax.tick_params(axis='x', which='minor', bottom=False, top=False)
+    ax.set_xlim(pd.Timestamp("2020-01-01"), pd.Timestamp("2100-01-01"))
+    ax.axhline(color='gray', linewidth=0.5)
+    
+    return fig, ax
+
+
+# %% editable=true slideshow={"slide_type": ""}
+OUTPUT.mkdir(exist_ok=True)
+
+#for var in chart_elements:
+for var in chart_elements:
+    subdf = df[['Scenario', 'Year', 'Group', var]].copy()
+
+    fig, ax = make_plot(var, subdf, chart_elements[var].get('fig_id'))
+    fig.savefig(OUTPUT / f"{var}.png", bbox_inches='tight')
+    plt.show()
+
+# %%
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+
+scale = 1
+width, height = mpl.rcParams["figure.figsize"]
+fig_legend, ax_legend = plt.subplots(figsize=(width*scale, height*scale), dpi=300)
+
+legend = {
+    'Median pathway': Line2D([0], [0], **(kwds['stat'] | kwds['Median'])),
+    'Ensemble of energy-constrained pathways': Rectangle((0, 0), 1, 1, **kwds['energy']),
+    'IMP-Low Demand (IMP-LD)': Line2D([0], [0], **(kwds['imp'] | kwds['IMP-LD'])),
+
+    'Full combination of median GHG-constrained pathways': Line2D([0], [0], **(kwds['emissions'] | kwds['tab2'])),
+    '5th-95th percentile ensemble': Rectangle((0, 0), 1, 1, **kwds['stat']),
+    'IMP-Sustainable Pathway (IMP-SP) ': Line2D([0], [0], **(kwds['imp'] | kwds['IMP-SP'])),
+
+    'Full combination of median energy-constrained pathways': Line2D([0], [0], **(kwds['energy'] | kwds['tab4'])),
+    'Ensemble of GHG-constrained pathways': Rectangle((0, 0), 1, 1, **kwds['emissions']),
+    'IMP-Renewables (IMP-Ren)': Line2D([0], [0], **(kwds['imp'] | kwds['IMP-Ren'])),
+}
+
+ax_legend.legend(handles=legend.values(), labels=legend.keys(), loc='center', ncol=3)
+ax_legend.axis('off')
+plt.subplots_adjust(top=1e-6, bottom=0)
+fig_legend.savefig("results/legend.png", bbox_inches='tight')
+
+plt.show()
