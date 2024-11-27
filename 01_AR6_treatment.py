6f8dc0e9e0a1af36273855174da8c25e03686d30
corralien <damien.corral@gmail.com>
2024-11-27 13:50:43 +0100

    code refactoring for 01

--- 01_AR6_treatment.py
+++ 01_AR6_treatment.py
@@ -18,72 +18,16 @@
 # The [AR6 Scenarios Database](https://data.ece.iiasa.ac.at/ar6/#/workspaces) ([Byers et al., 2022](https://data.ece.iiasa.ac.at/ar6/#)) is imported to retrieve the statistical values of Tables 3.2 and Table 3.4 from IPCC AR6 WGIII Chapter 3 ([Riahi et al., 2023](https://www.cambridge.org/core/books/climate-change-2022-mitigation-of-climate-change/mitigation-pathways-compatible-with-longterm-goals/7C750344E39ECA3BD5CB14156FCEEFE9)). The user can choose which scenarios' category to extract.
 
 # %%
-import pathlib
 import pandas as pd
-import os
+from zipfile import ZipFile
+import pathlib
 
-DATADIROUT = pathlib.Path('outputs')
-
-
-# %%
-def load_data(category, variables):
-    """Imports the database of scenarios and their variables according to the category entered by the user.
-
-    category (string): a category from the column 'Category' of the database (e.g., 'C1')
-    variables (list): a list of variables from the column 'Variable' of the database (e.g., ['Final Energy','Final Energy|Electricity']
-
-    Returns a dataframe.
-    """
-    #df = pd.read_csv('.\AR6_Scenarios_Database_World_v1.1\AR6_Scenarios_Database_World_v1.1.csv')
-    #df.to_parquet('.\AR6_Scenarios_Database_World_v1.1.parquet')
-    df = pd.read_parquet('.\data\AR6_Scenarios_Database_World_v1.1.parquet')
-
-    # Define the columns of the dataframe
-    cols = ['Model', 'Scenario', 'Region', 'Variable', 'Unit'] + [str(i) for i in range(2020, 2101, 10)]
-
-    # Select pairs of models and scenarios belonging to the user-defined category
-    cdict = pd.read_excel('.\data\AR6_Scenarios_Database_metadata_indicators_v1.1.xlsx', sheet_name='meta_Ch3vetted_withclimate')
-    df = df.merge(cdict, on=['Model', 'Scenario'], how='left')
-    df = df[df['Category'] == category]
-    df = df.drop(columns=['Category'])
-
-    df = df.loc[df['Variable'].isin(variables), cols]
-    df = pd.melt(df, id_vars=cols[:5], var_name='Year', value_name='Value')
-
-    return df
-
+DATADIR = pathlib.Path('data')
+DATADIR.mkdir(exist_ok=True)
 
 # %%
-def load_IMP(category):
-    """Select the Illustrative Mitigation Pathways of a given category with the name of the scenarios and their alias.
-
-    category (string): a category from the column 'Category' of the database (e.g., 'C1')
-
-    Returns a list of the full name of the category-specific IMPs and a dictionnary of the IMPs with their alias.
-    """
-    
-    df = pd.read_excel('.\data\AR6_Scenarios_Database_metadata_indicators_v1.1.xlsx', sheet_name='meta_Ch3vetted_withclimate')
-
-    # Define the columns of the dataframe
-    cols = ['Scenario', 'Category', 'IMP_marker']
-
-    # Extracts the IMPs
-    df = df.loc[df['Category'].isin([category]), cols]
-    df = df[df['IMP_marker'] != 'non-IMP']
-    IMP_Scen = df['Scenario']
-    IMP_Scen = IMP_Scen.values.tolist()
-
-    IMP = df.drop(columns=['Category'])
-    IMP = {str(key): 'IMP-' + value for key, value in zip(IMP.iloc[:, 0], IMP.iloc[:, 1])}
-    
-    return IMP_Scen, IMP
-
-
-# %% [markdown]
-# The following variables are either those given in Tables 3.2 and 3.4 of IPCC AR6 WGIII Chapter 3 (([Riahi et al., 2023](https://www.cambridge.org/core/books/climate-change-2022-mitigation-of-climate-change/mitigation-pathways-compatible-with-longterm-goals/7C750344E39ECA3BD5CB14156FCEEFE9)) or are furtherly computed to retrieve these variables.
-
-# %%
-# AR6 variables to be processed.
+# AR6 variables to be processed
+archive = DATADIR / "1668008030411-AR6_Scenarios_Database_World_ALL_CLIMATE_v1.1.csv.zip"
 variables = ['AR6 climate diagnostics|Infilled|Emissions|Kyoto Gases (AR6-GWP100)',
              'Final Energy',
              'Final Energy|Electricity',
@@ -114,15 +58,49 @@ variables = ['AR6 climate diagnostics|Infilled|Emissions|Kyoto Gases (AR6-GWP100
              'Emissions|N2O|Other',
              'Emissions|F-Gases']
 
-# %%
-# Load the AR6 Scenarios Database for a given category and set of variables
-category = 'C1'
-df = load_data(category,variables)
-IMP_Scen, IMP = load_IMP(category)
+# Import the database and metadata of scenarios and a subset of variables
+with ZipFile(archive) as zipfile:
+    # Process database
+    cols = ['Model', 'Scenario', 'Region', 'Variable', 'Unit'] + [str(i) for i in range(2020, 2101, 10)]
+    data = pd.read_csv(zipfile.open('AR6_Scenarios_Database_World_ALL_CLIMATE_v1.1.csv'), usecols=cols)
+    data = data[data['Variable'].isin(variables)]
+
+    # Process metadata
+    cols = ['Model', 'Scenario', 'Category', 'IMP_marker']
+    meta = pd.read_excel(zipfile.open('AR6_Scenarios_Database_metadata_indicators_v1.1.xlsx'), sheet_name='meta_Ch3vetted_withclimate', usecols=cols)
+
+# Combine dataframes
+cols = ['Model', 'Scenario', 'Region', 'Variable', 'Unit', 'Year', 'Value', 'Category', 'IMP_marker']
+df = meta.merge(data, on=['Model', 'Scenario'], how='right')
+df = pd.melt(df, id_vars=df.columns[:7], var_name='Year', value_name='Value')
+df.to_csv(DATADIR / 'AR6_Scenarios_Database_World_ALL_CLIMATE_subset_and_metadata_v1.1.csv', index=False)
+
+# %% [markdown]
+# The following variables are either those given in Tables 3.2 and 3.4 of IPCC AR6 WGIII Chapter 3 (([Riahi et al., 2023](https://www.cambridge.org/core/books/climate-change-2022-mitigation-of-climate-change/mitigation-pathways-compatible-with-longterm-goals/7C750344E39ECA3BD5CB14156FCEEFE9)) or are furtherly computed to retrieve these variables.
 
 # %%
-# Check that the number of scenarios within the explored category matches the IPCC records.s
-df[['Model', 'Scenario']].drop_duplicates().shape[0]
+# Load the AR6 Scenarios Database for a given category and set of variables
+cat = 'C1'
+dfc = df[df['Category'] == cat]
+
+# %%
+# Check that the number of scenarios within the explored category matches the IPCC records
+dfc[['Model', 'Scenario']].drop_duplicates().shape[0]  # 97 for C1
+
+
+# %%
+# Compute statictical and extract IMP scenarios
+def process_scenarios(df):
+    stat = (df.groupby('Year')['Value'].quantile([0.05, 0.50, 0.95])
+              .unstack().set_axis(pd.Index(['5th', 'Median', '95th'], name='Scenario'), axis=1)
+              .stack().rename('Value').swaplevel().sort_index().reset_index())
+
+    imp = (df[df['IMP_marker'] != 'non-IMP']
+             .assign(Scenario=lambda x: 'IMP-' + x['IMP_marker'])
+             .drop(columns=['IMP_marker', 'Model']))
+
+    return pd.concat([stat, imp], ignore_index=True)
+
 
 # %% [markdown]
 # ## Electricity share of final energy (ESFE)
@@ -130,54 +108,31 @@ df[['Model', 'Scenario']].drop_duplicates().shape[0]
 # %%
 # Filter necessary variables to compute the ratio of electricity in final energy demand globally
 var = ['Final Energy', 'Final Energy|Electricity']
-esfe = df.loc[df['Variable'].isin(var)]
+esfe = dfc[dfc['Variable'].isin(var)]
 
-# %%
-#Calculate the ratio of electricity in final energy
-esfe = pd.pivot_table(esfe, columns='Variable', index=['Model','Scenario','Year'], values='Value').reset_index().rename_axis(columns=None)
-esfe['Value']=esfe['Final Energy|Electricity']/esfe['Final Energy']
-esfe = esfe.drop(columns=['Final Energy','Final Energy|Electricity'])
+# Calculate the ratio of electricity in final energy
+calc_esfe = lambda x: x['Final Energy|Electricity'] / x['Final Energy']
 
-# %%
-# Extract the values for IMPs only
-esfe_imp = esfe.loc[esfe['Scenario'].isin(IMP_Scen), ['Scenario', 'Year', 'Value']]
-esfe_imp['Scenario'] = esfe_imp['Scenario'].replace(IMP)
+esfe = (pd.pivot(esfe, columns='Variable', index=['IMP_marker', 'Model', 'Scenario', 'Year'], values='Value')
+          .sort_values(['Scenario', 'Year'])
+          .reset_index().rename_axis(columns=None)
+          .assign(Value=calc_esfe)
+          .drop(columns=var))
 
-# %%
-#Calculates the median and the 5th and 95th percentiles
-centiles = esfe.groupby(['Year'])['Value'].quantile([0.05, 0.50, 0.95]).unstack(level=-1).reset_index()
-centiles.columns = ['Year', '5th', 'Median', '95th']
-centiles
-esfe = pd.melt(centiles, id_vars=['Year'], var_name='Scenario', value_name='Value')
-
-# %%
-# Merge the centiles with the IMPs
-esfe = pd.merge(esfe, esfe_imp, how='outer', sort='Scenario').sort_values(by=['Scenario','Year'], ascending=[False, True])
+esfe = process_scenarios(esfe).assign(Value=lambda x: x['Value'].round(2))  # percentage
 
 # %% [markdown]
 # ## Greenhouse gases (GHG)
 
 # %%
-# Filter only the necessary variable for GHG emissions
+# Filter only the necessary variable
 var = ['AR6 climate diagnostics|Infilled|Emissions|Kyoto Gases (AR6-GWP100)']
-ghg = df.loc[df['Variable'].isin(var)]
+ghg = dfc[dfc['Variable'].isin(var)]
 
-# %%
-# Extract the values for IMPs only
-ghg_imp = ghg.loc[ghg['Scenario'].isin(IMP_Scen), ['Scenario', 'Year', 'Value']]
-ghg_imp['Scenario'] = ghg_imp['Scenario'].replace(IMP)
+ghg = (ghg.drop(columns=['Category', 'Region', 'Unit', 'Variable'])
+          .sort_values(['Scenario', 'Year']))
 
-# %%
-# Calculates the median and the 5th and 95th percentiles
-centiles = ghg.groupby(['Year'])['Value'].quantile([0.05, 0.50, 0.95]).unstack(level=-1).reset_index()
-centiles.columns = ['Year', '5th', 'Median', '95th']
-centiles
-ghg = pd.melt(centiles, id_vars=['Year'], var_name='Scenario', value_name='Value')
-
-# %%
-# Merge the centiles with the IMPs
-ghg = pd.merge(ghg, ghg_imp, how='outer', sort='Scenario').sort_values(by=['Scenario','Year'], ascending=[False, True])
-ghg['Value'] = ghg['Value'].round(0)/1000 # to obtain values in Gt
+ghg = process_scenarios(ghg).assign(Value=lambda x: x['Value'].round(0).div(1000))  # to obtain values in Gt
 
 # %% [markdown]
 # ## Final energy demand (FED)
@@ -185,24 +140,12 @@ ghg['Value'] = ghg['Value'].round(0)/1000 # to obtain values in Gt
 # %%
 # Filter only the necessary variable
 var = ['Final Energy']
-fed = df.loc[df['Variable'].isin(var)]
+fed = dfc[dfc['Variable'].isin(var)]
 
-# %%
-# Extract the values for IMPs only
-fed_imp = fed.loc[fed['Scenario'].isin(IMP_Scen), ['Scenario', 'Year', 'Value']]
-fed_imp['Scenario'] = fed_imp['Scenario'].replace(IMP)
+fed = (fed.drop(columns=['Category', 'Region', 'Unit', 'Variable'])
+          .sort_values(['Scenario', 'Year']))
 
-# %%
-#Calculates the median and the 5th and 95th percentiles
-centiles = fed.groupby(['Year'])['Value'].quantile([0.05, 0.50, 0.95]).unstack(level=-1).reset_index()
-centiles.columns = ['Year', '5th', 'Median', '95th']
-centiles
-fed = pd.melt(centiles, id_vars=['Year'], var_name='Scenario', value_name='Value')
-
-# %%
-# Merge the centiles with the IMPs
-fed = pd.merge(fed, fed_imp, how='outer', sort='Scenario').sort_values(by=['Scenario','Year'], ascending=[False, True])
-fed['Value'] = fed['Value'].round(0)
+fed = process_scenarios(fed).assign(Value=lambda x: x['Value'].round(0))
 
 # %% [markdown]
 # ## Fossil CO<sub>2</sub> (CCSFOS)
@@ -210,55 +153,31 @@ fed['Value'] = fed['Value'].round(0)
 # %%
 # Filter only the necessary variable
 var = ['Carbon Sequestration|CCS|Fossil']
-ccsfos = df.loc[df['Variable'].isin(var)]
+ccsfos = dfc.loc[dfc['Variable'].isin(var)]
 
-# %%
-# Extract the values for IMPs only
-ccsfos_imp = ccsfos.loc[ccsfos['Scenario'].isin(IMP_Scen), ['Scenario', 'Year', 'Value']]
-ccsfos_imp['Scenario'] = ccsfos_imp['Scenario'].replace(IMP)
+ccsfos = (ccsfos.drop(columns=['Category', 'Region', 'Unit', 'Variable'])
+                .sort_values(['Scenario', 'Year']))
 
-# %%
-# Calculates the median and the 5th and 95th percentiles
-centiles = ccsfos.groupby(['Year'])['Value'].quantile([0.05, 0.50, 0.95]).unstack(level=-1).reset_index()
-centiles.columns = ['Year', '5th', 'Median', '95th']
-centiles
-ccsfos = pd.melt(centiles, id_vars=['Year'], var_name='Scenario', value_name='Value')
-
-# %%
-# Merge the centiles with the IMPs
-ccsfos = pd.merge(ccsfos, ccsfos_imp, how='outer', sort='Scenario').sort_values(by=['Scenario','Year'], ascending=[False, True])
-ccsfos['Value'] = ccsfos['Value'].round(0)/1000 # to obtain values in Gt
+ccsfos = process_scenarios(ccsfos).assign(Value=lambda x: x['Value'].round(0))
 
 # %% [markdown]
 # ## CO<sub>2</sub> intensity of electricity (CO2ELC)
 
 # %%
 # Filter variables needed to calculate the ratio of carbon content of electricity to electricity generation
-var = ['Secondary Energy|Electricity','Emissions|CO2|Energy|Supply|Electricity']
-co2elc = df.loc[df['Variable'].isin(var)]
+var = ['Secondary Energy|Electricity', 'Emissions|CO2|Energy|Supply|Electricity']
+co2elc = dfc[dfc['Variable'].isin(var)]
 
-# %%
-#Calculate the carbon content of electricity
-co2elc = pd.pivot_table(co2elc, columns='Variable', index=['Model','Scenario','Year'], values='Value').reset_index().rename_axis(columns=None)
-co2elc['Value']=co2elc['Emissions|CO2|Energy|Supply|Electricity']/co2elc['Secondary Energy|Electricity']*3.6
-co2elc = co2elc.drop(columns=var)
+# Calculate the carbon content of electricity
+calc_co2elc = lambda x: x['Emissions|CO2|Energy|Supply|Electricity'] / x['Secondary Energy|Electricity'] * 3.6
 
-# %%
-# Extract the values for IMPs only
-co2elc_imp = co2elc.loc[co2elc['Scenario'].isin(IMP_Scen), ['Scenario', 'Year', 'Value']]
-co2elc_imp['Scenario'] = co2elc_imp['Scenario'].replace(IMP)
+co2elc = (pd.pivot(co2elc, columns='Variable', index=['IMP_marker', 'Model', 'Scenario', 'Year'], values='Value')
+          .sort_values(['Scenario', 'Year'])
+          .reset_index().rename_axis(columns=None)
+          .assign(Value=calc_co2elc)
+          .drop(columns=var))
 
-# %%
-#Calculates the median and the 5th and 95th percentiles
-centiles = co2elc.groupby(['Year'])['Value'].quantile([0.05, 0.50, 0.95]).unstack(level=-1).reset_index()
-centiles.columns = ['Year', '5th', 'Median', '95th']
-centiles
-co2elc = pd.melt(centiles, id_vars=['Year'], var_name='Scenario', value_name='Value')
-
-# %%
-# Merge the centiles with the IMPs
-co2elc = pd.merge(co2elc, co2elc_imp, how='outer', sort='Scenario').sort_values(by=['Scenario','Year'], ascending=[False, True])
-co2elc = co2elc.dropna(how='any')
+co2elc = process_scenarios(co2elc).assign(Value=lambda x: x['Value'].round(0))
 
 # %% [markdown]
 # ## Low-carbon share of primary energy (LCSPE)
@@ -266,34 +185,19 @@ co2elc = co2elc.dropna(how='any')
 
 # %%
 # Filter variables needed to calculate the low-carbon share of primary energy
-var = ['Primary Energy','Primary Energy|Fossil|w/ CCS','Primary Energy|Nuclear','Primary Energy|Renewables (incl. Biomass)']
-lcspe = df.loc[df['Variable'].isin(var)]
+var = ['Primary Energy', 'Primary Energy|Fossil|w/ CCS','Primary Energy|Nuclear', 'Primary Energy|Renewables (incl. Biomass)']
+lcspe = dfc.loc[dfc['Variable'].isin(var)]
 
-# %%
-#Calculate the low-carbon share of primary energy
-lcspe = pd.pivot_table(lcspe, columns='Variable', index=['Model','Scenario','Year'], values='Value').reset_index().rename_axis(columns=None)
-lcspe.fillna(0)
-lcspe['Value'] = (lcspe['Primary Energy|Fossil|w/ CCS']+lcspe['Primary Energy|Nuclear']+lcspe['Primary Energy|Renewables (incl. Biomass)'])/lcspe['Primary Energy']
-lcspe.fillna(0)
-lcspe = lcspe.drop(columns=var)
-lcspe = lcspe.dropna(how='any')
+# Calculate the low-carbon share of primary energy
+calc_lcspe = lambda x: x[['Primary Energy|Fossil|w/ CCS', 'Primary Energy|Nuclear', 'Primary Energy|Renewables (incl. Biomass)']].sum(axis=1) / x['Primary Energy']
 
-# %%
-# Extract the values for IMPs only
-lcspe_imp = lcspe.loc[lcspe['Scenario'].isin(IMP_Scen), ['Scenario', 'Year', 'Value']]
-lcspe_imp['Scenario'] = lcspe_imp['Scenario'].replace(IMP)
+lcspe = (pd.pivot(lcspe, columns='Variable', index=['IMP_marker', 'Model', 'Scenario', 'Year'], values='Value')
+           .sort_values(['Scenario', 'Year'])
+           .reset_index().rename_axis(columns=None)
+           .assign(Value=calc_lcspe)
+           .drop(columns=var))
 
-# %%
-#Calculates the median and the 5th and 95th percentiles
-centiles = lcspe.groupby(['Year'])['Value'].quantile([0.05, 0.50, 0.95]).unstack(level=-1).reset_index()
-centiles.columns = ['Year', '5th', 'Median', '95th']
-centiles
-lcspe = pd.melt(centiles, id_vars=['Year'], var_name='Scenario', value_name='Value')
-
-# %%
-# Merge the centiles with the IMPs
-lcspe = pd.merge(lcspe, lcspe_imp, how='outer', sort='Scenario').sort_values(by=['Scenario','Year'], ascending=[False, True])
-lcspe = lcspe.dropna(how='any')
+lcspe = process_scenarios(lcspe).assign(Value=lambda x: x['Value'].round(2))  # percentage
 
 # %% [markdown]
 # ## Non-energy GHG emissions (NONNRG)
@@ -301,36 +205,20 @@ lcspe = lcspe.dropna(how='any')
 
 # %%
 # Filter only necessary variables
-var = ['Emissions|CO2','Emissions|CO2|Energy',
-        'Emissions|CH4','Emissions|CH4|Energy',
-        'Emissions|N2O','Emissions|N2O|Energy',
-        'Emissions|F-Gases']
-nonnrg = df.loc[df['Variable'].isin(var)]
+var = ['Emissions|CO2','Emissions|CO2|Energy', 'Emissions|CH4','Emissions|CH4|Energy',
+        'Emissions|N2O','Emissions|N2O|Energy', 'Emissions|F-Gases']
+nonnrg = dfc.loc[dfc['Variable'].isin(var)]
 
-# %%
-#Calculate the non-energy GHG emissions
-nonnrg = pd.pivot_table(nonnrg, columns='Variable', index=['Model','Scenario','Year'], values='Value').reset_index().rename_axis(columns=None)
-nonnrg['Value'] = nonnrg['Emissions|CO2'] - nonnrg['Emissions|CO2|Energy'] + (nonnrg['Emissions|CH4'] - nonnrg['Emissions|CH4|Energy'])*29.8 + (nonnrg['Emissions|N2O'] - nonnrg['Emissions|N2O|Energy'])*273/1000 + nonnrg['Emissions|F-Gases']
+# Calculate the non-energy GHG emissions
+calc_nonnrg = lambda x: x['Emissions|CO2'] - x['Emissions|CO2|Energy'] + (x['Emissions|CH4'] - x['Emissions|CH4|Energy'])*29.8 + (x['Emissions|N2O'] - x['Emissions|N2O|Energy'])*0.273 + x['Emissions|F-Gases']
 
+nonnrg = (pd.pivot(nonnrg, columns='Variable', index=['IMP_marker', 'Model', 'Scenario', 'Year'], values='Value')
+            .sort_values(['Scenario', 'Year'])
+            .reset_index().rename_axis(columns=None)
+            .assign(Value=calc_nonnrg)
+            .drop(columns=var))
 
-# %%
-# Extract the values for IMPs only
-nonnrg_imp = nonnrg.loc[nonnrg['Scenario'].isin(IMP_Scen), ['Model','Scenario', 'Year', 'Value']]
-nonnrg_imp['Scenario'] = nonnrg_imp['Scenario'].replace(IMP)
-nonnrg_imp = nonnrg_imp.drop(columns=['Model'])
-
-# %%
-#Calculates the median and the 5th and 95th percentiles
-centiles = nonnrg.groupby(['Year'])['Value'].quantile([0.05, 0.50, 0.95]).unstack(level=-1).reset_index()
-centiles.columns = ['Year', '5th', 'Median', '95th']
-centiles
-nonnrg = pd.melt(centiles, id_vars=['Year'], var_name='Scenario', value_name='Value')
-
-# %%
-# Merge the centiles with the IMPs
-nonnrg = pd.merge(nonnrg, nonnrg_imp, how='outer', sort='Scenario').sort_values(by=['Scenario','Year'], ascending=[False, True])
-nonnrg = nonnrg.dropna(how='any')
-nonnrg['Value'] = nonnrg['Value'].round(0)/1000 # to obtain values in Gt
+nonnrg = process_scenarios(nonnrg).assign(Value=lambda x: x['Value'].round(0).div(1000))  # to obtain values in Gt
 
 # %% [markdown]
 # ## CSV output
@@ -342,7 +230,8 @@ nonnrg['Value'] = nonnrg['Value'].round(0)/1000 # to obtain values in Gt
 dfs = {'ghg': ghg, 'lcspe': lcspe, 'fed': fed, 'esfe': esfe,
        'co2elc': co2elc, 'ccsfos': ccsfos, 'nonnrg': nonnrg}
 out = (pd.concat(dfs, names=['Variable']).reset_index('Variable')
-        .pivot_table(index=['Scenario', 'Year'], columns='Variable', values='Value'))
-out.to_csv(DATADIROUT / 'constraints.csv')
+        .pivot_table(index=['Scenario', 'Year'], columns='Variable', values='Value', sort=False))
+out.to_csv(DATADIR / 'constraints.csv')
 
 # %%
+out
